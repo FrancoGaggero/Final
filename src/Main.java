5bf039e28ebd19d10e5a72d480f91dc61d7b7797
FrancoGaggero <franco.gaggero@davinci.edu.ar>
2024-11-11 10:49:50 -0300

    muestro primer menu

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 		JOptionPane.showMessageDialog(null, "hola");
 		
 		
-	
+	int opcion= JOptionPane.showOptionDialog(null, "", null, 0, 0, null, Menuprincipal.values(), Menuprincipal.values()[0]);
 		
 	}
 
